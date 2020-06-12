70201fbb98d805dd429fae56ef8c14bec59da77f
John Richter <johndavidrichter@gmail.com>
2020-06-12 13:01:50 -0400

    fix infinite loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './modules/home-page/home-page/home-page.component';
 import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
